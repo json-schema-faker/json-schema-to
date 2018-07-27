9b58d73c7522a8a781f08ba34a7508b82b0249ca
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-07-27 01:50:09 -0500

    Tests are passing the GRPC integration

--- test/example.test.js
+++ test/example.test.js
@@ -1,3 +1,9 @@
+const mockFs = require('mock-fs');
+const expect = require('chai').expect;
+
+const grpcLibrary = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+
 const jsf = require('json-schema-faker');
 
 jsf.option({
@@ -19,6 +25,12 @@ const refs = [
     id: 'ItemValue',
     $ref: 'external.schema.json#/definitions/justAString',
   },
+  {
+    id: 'Empty',
+  },
+  {
+    id: 'someValue',
+  }
 ];
 
 const schema = {
@@ -42,11 +54,50 @@ const schema = {
 
 describe('Test', () => {
   it('OK', async () => {
-    const results = await jst.parse(__dirname, refs, schema);
-    const output = jst.generate(results);
+    const package = {
+      name: 'foo-bar',
+      refs: ['external'],
+      calls: [
+        // FIXME: provide this through schemas too?
+        { req: 'something', resp: 'someValue' },
+      ],
+    };
 
-    console.log(JSON.stringify(jsf(schema, refs, __dirname), null, 2));
-    console.log(JSON.stringify(results, null, 2));
-    console.log(output);
+    const models = await jst.parse(__dirname, refs, schema);
+    const code = jst.generate(package, models, jst.protobufDefs);
+
+    mockFs({
+      'generated.proto': Buffer.from(code),
+    });
+
+    try {
+      const protoOptions = {};
+      const packageDefinition = protoLoader.loadSync('generated.proto', protoOptions);
+      const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
+
+      expect(packageObject.fooBar.FooBar).not.to.be.undefined;
+
+      expect(code).not.to.contain('undefined');
+      expect(code).not.to.contain('null');
+      expect(code).not.to.contain('NaN');
+
+      expect(code).to.contain('repeated string values = 3;');
+      expect(code).not.to.contain('message ItemValue');
+    } catch (e) {
+      const matches = e.message.match(/line (\d+)/);
+
+      if (matches) {
+        console.log(e.message);
+        console.log(code.trim().split('\n')
+          .map((x, l) => `${(matches[1] - 1) === l ? ' >' : '  '} ${`00${l + 1}`.substr(-2)} ${x}`)
+          .join('\n'));
+      } else {
+        console.log(code);
+      }
+
+      throw e;
+    } finally {
+      mockFs.restore();
+    }
   });
 });
