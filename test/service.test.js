15ae7348a27938259aa8662716e5bb0b8818fb44
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-08-03 18:23:36 -0500

    Rename serviceDefinition => service; fix

--- test/service.test.js
+++ test/service.test.js
@@ -14,14 +14,14 @@ describe('Service', () => {
       });
 
       it('should fail on invalid definitions', () => {
-        expect(() => new Service({ serviceDefinition: true })).to.throw('Invalid service definition, given true');
-        expect(() => new Service({ serviceDefinition: [] })).to.throw('Invalid service definition, given []');
+        expect(() => new Service({ service: true })).to.throw('Invalid service definition, given true');
+        expect(() => new Service({ service: [] })).to.throw('Invalid service definition, given []');
       });
 
       it('should fail on invalid schema definitions', () => {
-        expect(() => new Service({ serviceDefinition: {} })).to.throw('Invalid schema identifier, given {}');
-        expect(() => new Service({ serviceDefinition: {}, schema: NaN })).to.throw('Invalid schema identifier, given { schema: NaN }');
-        expect(() => new Service({ serviceDefinition: {}, schema: { id: -1 } })).to.throw('Invalid schema identifier, given { schema: { id: -1 } }');
+        expect(() => new Service({ service: {} })).to.throw('Invalid schema identifier, given {}');
+        expect(() => new Service({ service: {}, schema: NaN })).to.throw('Invalid schema identifier, given { schema: NaN }');
+        expect(() => new Service({ service: {}, schema: { id: -1 } })).to.throw('Invalid schema identifier, given { schema: { id: -1 } }');
       });
     });
   });
